f4392b6f38431b1aa594af46c99e4ce98de559b9
Keijo Nierula <keijo@keijos-mbp.home>
2022-05-22 18:41:53 +0200

    update import script and add sql generate script to git

--- importdata.py
+++ importdata.py
@@ -1,23 +1,204 @@
-import xlrd
 import csv
 import sqlite3
 
-#book = xlrd.open_workbook("../../RealEstate_California-adapted.xlsx")
-#sheet = book.sheet_by_name("Tabelle1")
 
-csvfile = open('../../RealEstate_California-adapted-new.csv',newline='') # `with` statement available in 2.5+# csv.DictReader uses first line in file for column headings by default
+def getCsv():
+    csvfile = open('../../RealEstate_California-adapted-new.csv',newline='') # `with` statement available in 2.5+# csv.DictReader uses first line in file for column headings by default
+    return csv.reader(csvfile, delimiter=',', quotechar='|')
 
-priceList = set()
-pricePerSquareFootList = set()
-csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+# Country
+countryList = set()
+zipcodeList = set()
+cityList = set()
+currencyList = set()
+eventList = set()
+homeTypeList = set()
+lotAreaUnitList = set()
+countyList = set()
+stateList = set()
+
+csvreader = getCsv()
 for row in csvreader:
-    #print(', '.join(row))
-    priceList.add(row[6])
-    pricePerSquareFootList.add(row[7])
+    countryList.add(row[1])
+    zipcodeList.add(row[12])
+    cityList.add(row[8])
+    currencyList.add(row[17])
+    eventList.add(row[5])
+    homeTypeList.add(row[32])
+    lotAreaUnitList.add(row[20])
+    countyList.add(row[33])
+    stateList.add(row[9])
 
-print(priceList)
-print(pricePerSquareFootList)
+database = sqlite3.connect('../wdda_sem_db1.db')
+cursor = database.cursor()
 
+#insert country
+insertQuery = "INSERT INTO Country (Country) VALUES (?)"
+for country in countryList:
+    # Execute sql Query
+    if country != 'country':
+        cursor.execute(insertQuery, (country,))
+
+
+newreader = getCsv()
+# insert coordinates
+insertQuery = "INSERT INTO Coordinates (Longitude, Latitude, HasBadGeoCoordinates) VALUES (?, ?, ?)"
+for row in newreader:
+    longitude = row[13]
+    latitude = row[14]
+    hasBadGeoCode = row[15]
+    if (longitude != 'longitude') & (latitude != 'latitude') & (hasBadGeoCode != 'hasBadGeocode'):
+        cursor.execute(insertQuery, (longitude, latitude, hasBadGeoCode,))
+
+
+# insert Zipcodes
+insertQuery = "INSERT INTO Zipcode (Zipcode) VALUES (?)"
+for zipcode in zipcodeList:
+    if zipcode != 'zipcode':
+        cursor.execute(insertQuery, (zipcode,))
+
+
+# insert Cities
+insertQuery = "INSERT INTO City (City) VALUES (?)"
+for city in cityList:
+    if city != 'city':
+        cursor.execute(insertQuery, (city,))
+
+
+# insert Currencies
+insertQuery = "INSERT INTO Currency (Currency) VALUES (?)"
+for currency in currencyList:
+    if currency != 'currency':
+        cursor.execute(insertQuery, (currency,))
+
+
+# insert Events
+insertQuery = "INSERT INTO Event (Event) VALUES (?)"
+for event in eventList:
+    if event != 'event':
+        cursor.execute(insertQuery, (event,))
+
+# insert HomeType
+insertQuery = "INSERT INTO HomeType (HomeType) VALUES (?)"
+for homeType in homeTypeList:
+    if homeType != 'homeType':
+        cursor.execute(insertQuery, (homeType,))
+
+
+# insert LotAreaUnit
+insertQuery = "INSERT INTO LotAreaUnit (LotAreaUnit) VALUES (?)"
+for lotAreUnit in lotAreaUnitList:
+    if lotAreUnit != 'lotAreUnits':
+        cursor.execute(insertQuery, (lotAreUnit,))
+
+# insert County
+insertQuery = "INSERT INTO County (County) VALUES (?)"
+for county in countyList:
+    if county != 'county':
+        cursor.execute(insertQuery, (county,))
+
+# insert State
+#TODO: Insert from select
+insertQuery = "INSERT INTO State (State, FK_Country) VALUES (?, ?)"
+for state in stateList:
+    if state != 'state':
+        cursor.execute(insertQuery, (state, 1))
+
+
+
+database.commit()
+
+
+def getCityId(city):
+    selectQuery = """SELECT ID FROM City WHERE City = ?"""
+    a = cursor.execute(selectQuery, (city,))
+    return a.fetchone()
+
+def getStateId(state):
+    selectQuery = """SELECT ID FROM State WHERE State = ?"""
+    a = cursor.execute(selectQuery, (state,))
+    return a.fetchone()
+
+def getCoordinatesId(longitude, latitude, hasBadGeoCode):
+    selectQuery = """SELECT ID FROM Coordinates WHERE Longitude = ? AND Latitude = ? AND HasBadGeoCoordinates = ?"""
+    a = cursor.execute(selectQuery, (longitude, latitude, hasBadGeoCode,))
+    return a.fetchone()
+
+def getCountyId(county):
+    selectQuery = """SELECT ID FROM County WHERE County = ?"""
+    a = cursor.execute(selectQuery, (county,))
+    return a.fetchone()
+
+def getzipcodeid(zipcode):
+    selectQuery = """SELECT ID FROM Zipcode WHERE Zipcode = ?"""
+    a = cursor.execute(selectQuery, (zipcode,))
+    return a.fetchone()
+
+# insert State
+csv = getCsv()
+insertQuery = "INSERT INTO Address (Address, FK_City, FK_State, FK_Zipcode, FK_Coordinates, FK_County) VALUES (?, ?, ?, ?, ?, ?)"
+addressList = []
+for row in csv:
+    address = row[11]
+    city = row[8]
+    state = row[9]
+    longitude = row[13]
+    latitude = row[14]
+    hasBadGeoCode = row[15]
+    county = row[33]
+    zipcode = row[12]
+    if address != 'streetAddress':
+        cityId = getCityId(city)
+        stateId = getStateId(state)
+        coordinatesId = getCoordinatesId(longitude, latitude, hasBadGeoCode)
+        countyId = getCountyId(county)
+        zipcodeId = getzipcodeid(zipcode)
+        addressList.append((address, cityId, stateId, zipcodeId, coordinatesId, countyId))
+
+cursor.executemany(insertQuery, addressList)
+
+database.commit()
+
+selectQuery = """SELECT * FROM Address"""
+a = cursor.execute(selectQuery)
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+print(len(rows))
+
+cursor.close()
+
+database.close()
+
+
+
+
+
+
+# print(countryList)
+# #print(coordinatesLatitudeList)
+# print(coordinatesLongitudeList)
+# print(hasBadGeoCodesList)
+# print(zipcodeList)
+# print(cityList)
+# print(currencyList)
+# print(eventList)
+# print(homeTypeList)
+# print(lotAreaUnitList)
+
+
+
+
+
+# 0. Normalisierte Tabellen manuell in SQL abfüllen mit INSERT
+# 1. wir iterieren das ganze csv für sömtliche Daten
+# 2. sämtliche daten abfüllen
+# 3. Foreign Keys korrekt setzen innerhalb Schlaufe
+# 4. done
 
 
 
---
+++ semaufgabe_generate_tables.sql
@@ -0,0 +1,156 @@
+PRAGMA foreign_keys=off;
+DROP TABLE IF EXISTS `House`;
+DROP TABLE IF EXISTS `Address`;
+DROP TABLE IF EXISTS `Zipcode`;
+DROP TABLE IF EXISTS `State`;
+DROP TABLE IF EXISTS `Country`;
+DROP TABLE IF EXISTS `Coordinates`;
+DROP TABLE IF EXISTS `Price`;
+DROP TABLE IF EXISTS `City`;
+DROP TABLE IF EXISTS `Properties`;
+DROP TABLE IF EXISTS `LivingArea`;
+DROP TABLE IF EXISTS `LotAreaUnit`;
+DROP TABLE IF EXISTS `HomeType`;
+DROP TABLE IF EXISTS `Currency`;
+DROP TABLE IF EXISTS `Event`;
+DROP TABLE IF EXISTS `County`;
+PRAGMA foreign_keys=on;
+
+CREATE TABLE `City` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `City` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `Zipcode` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `Zipcode` INTEGER NOT NULL
+);
+
+CREATE TABLE `Coordinates` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `Longitude` FLOAT NOT NULL,
+    `Latitude` FLOAT NOT NULL,
+    `HasBadGeoCoordinates` BOOL NOT NULL
+);
+
+CREATE TABLE `Country` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `Country` VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE 'County' (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `County` VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE `State` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `State` VARCHAR(50) NOT NULL,
+    `FK_Country` INTEGER NOT NULL,
+    FOREIGN KEY ('FK_Country') REFERENCES Country (ID)
+);
+
+CREATE TABLE `Address` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `Address` VARCHAR(100) NOT NULL,
+    `FK_City` INTEGER NOT NULL,
+    `FK_State` INTEGER NOT NULL,
+    `FK_Zipcode` INTEGER NOT NULL,
+    `FK_Coordinates` INTEGER NOT NULL,
+    `FK_County`INTEGER NOT NULL,
+    FOREIGN KEY ('FK_City') REFERENCES City (ID),
+    FOREIGN KEY ('FK_State') REFERENCES State (ID),
+    FOREIGN KEY ('FK_Zipcode') REFERENCES Zipcode (ID),
+    FOREIGN KEY ('FK_Coordinates') REFERENCES Coordinates (ID),
+    FOREIGN KEY ('FK_County') REFERENCES County (ID)
+);
+
+CREATE TABLE `Currency` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `Currency` VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE `Price` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `Price` FLOAT NOT NULL,
+    `FK_Currency` INTEGER NOT NULL,
+    FOREIGN KEY ('FK_Currency') REFERENCES Currency (ID)
+);
+
+CREATE TABLE `LotAreaUnit` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `LotAreaUnit` VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE `LivingArea` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `LivingArea` INTEGER NOT NULL,
+    `LivingAreaValue` INTEGER NOT NULL,
+    `FK_LotAreaUnit` INTEGER NOT NULL,
+    FOREIGN KEY ('FK_LotAreaUnit') REFERENCES LotAreaUnit (ID)
+);
+
+CREATE TABLE `HomeType` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `HomeType` VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE `Event` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `Event` VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE `Properties` (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `Bathrooms` INTEGER NOT NULL,
+    `Bedrooms` INTEGER NOT NULL,
+    `Buildingarea` INTEGER NOT NULL,
+    `Parking` BOOL NOT NULL,
+    `HasGarage` BOOL NOT NULL,
+    `Garagespaces` INTEGER NOT NULL,
+    `Levels` VARCHAR(10) NOT NULL,
+    `Pool` BOOL NOT NULL,
+    `Spa` BOOL NOT NULL,
+    `IsNewConstruction` BOOL NOT NULL,
+    `HasPetsAllowed` BOOL NOT NULL,
+    `PricePerSquareFoot` VARCHAR(10) NOT NULL,
+    `YearBuilt` VARCHAR(10) NOT NULL,
+    'IsForAuction' BOOL NOT NULL,
+    'IsBankOwned' BOOL NOT NULL,
+    `FK_HomeType` INTEGER NOT NULL,
+    'FK_Event' INTEGER NOT NULL,
+    FOREIGN KEY ('FK_HomeType') REFERENCES HomeType (ID),
+    FOREIGN KEY ('FK_Event') REFERENCES Event (ID)
+);
+
+
+CREATE TABLE `House` (
+    `ID` INTEGER NOT NULL,
+    `Description` VARCHAR(10000),
+    `FK_Price` INTEGER NOT NULL,
+    `FK_Address` INTEGER NOT NULL,
+    `FK_Properties` INTEGER NOT NULL,
+    `FK_LivingArea` INTEGER NOT NULL,
+    PRIMARY KEY (`ID`),
+    FOREIGN KEY ('FK_Price') REFERENCES Price (ID),
+    FOREIGN KEY ('FK_Address') REFERENCES Address (ID),
+    FOREIGN KEY ('FK_Properties') REFERENCES Properties (ID),
+    FOREIGN KEY ('FK_LivingArea') REFERENCES LivingArea (ID)
+);
+
+
+SELECT * FROM  `House`;
+SELECT * FROM  `Address`;
+SELECT * FROM  `Zipcode`;
+SELECT * FROM  `State`;
+SELECT * FROM  `Country`;
+SELECT * FROM  `Coordinates`;
+SELECT * FROM  `Price`;
+SELECT * FROM  `City`;
+SELECT * FROM  `Properties`;
+SELECT * FROM  `LivingArea`;
+SELECT * FROM  `LotAreaUnit`;
+SELECT * FROM  `HomeType`;
+SELECT * FROM  `Currency`;
+SELECT * FROM  `Event`;
+SELECT * FROM  `County`;
