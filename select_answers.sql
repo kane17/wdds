236951f0d0351fceffc06a669be87eb01f00cf2b
Keijo Nierula <keijo@Keijos-MacBook-Pro.local>
2022-06-04 18:44:19 +0200

    select nr. 6 also seems to work now

--- select_answers.sql
+++ select_answers.sql
@@ -42,90 +42,25 @@ WHERE ((SELECT COUNT(DISTINCT Address.FK_County) FROM Address WHERE Address.FK_C
 SELECT c.City, ROUND(AVG(p.Price), 2) AS 'Durchschnittlicher Preis' FROM House h
     LEFT JOIN Address a ON a.ID = h.FK_Address
     LEFT JOIN Price p ON h.FK_Price = p.ID
-    LEFT JOIN City c ON c.ID = a.FK_City GROUP BY c.City;
+    LEFT JOIN City c ON c.ID = a.FK_City GROUP BY c.City ORDER BY ROUND(AVG(p.Price), 2) DESC;
+
+
+-- Aufgabe 5
+SELECT c.City, ROUND(AVG(prop.PricePerSquareFoot), 2) AS PricePerSquareFoot FROM House h
+    LEFT JOIN Address a ON a.ID = h.FK_Address
+    LEFT JOIN City c ON c.ID = a.FK_City
+    LEFT JOIN Properties prop ON prop.ID = h.FK_Properties
+    WHERE CAST(PricePerSquareFoot AS FLOAT) > CAST((SELECT ROUND(AVG(prop.PricePerSquareFoot), 2) AS 'Preis Pro Quadratmeter'
+    FROM House h LEFT JOIN Properties prop ON prop.ID = h.FK_Properties WHERE prop.PricePerSquareFoot != 'N/A') AS FLOAT) GROUP BY c.City;
 
 
 
-WHERE ((SELECT COUNT(DISTINCT Address.FK_County) FROM Address WHERE Address.FK_City=c.ID) > 1) ORDER BY DESC;
-
-SELECT avg(p.Price) AS avgPrice, c.City FROM House h
-INNER JOIN Price p ON h.FK_Price = p.ID
-INNER JOIN Address a ON a.ID = h.FK_Address
-INNER JOIN City c ON c.ID = a.FK_City
-WHERE (SELECT DISTINCT c.City FROM City) ORDER BY avgPrice DESC;
-
-
-
-
-
-
-
-
-
-
-SELECT c.City FROM City c
-    INNER JOIN Address a ON a.FK_City = c.ID
-    INNER JOIN County co ON co.ID = a.FK_County
-    WHERE (SELECT COUNT(*) FROM Address WHERE Address.FK_County > 1)
-
-SELECT c.City FROM City c
-   INNER JOIN Address a ON a.FK_City = c.ID
-   INNER JOIN County co ON co.ID = a.FK_County GROUP BY a.FK_COUNTY
-
-
-SELECT * FROM Address a
-    INNER JOIN County co ON co.ID = a.FK_County
-    INNER JOIN City c ON c.ID = a.FK_City;
-
-
-SELECT * FROM Address a
-    INNER JOIN County co ON co.ID = a.FK_County
-    INNER JOIN City c ON c.ID = a.FK_City;
-
-
-
-
-
-
-SELECT c.City, co.County, a.FK_City, a.FK_County FROM City c
-                                                          INNER JOIN Address a ON a.FK_City = c.ID
-                                                          INNER JOIN County co ON co.ID = a.FK_County
-WHERE  (SELECT DISTINCT Address.FK_County FROM Address WHERE Address.FK_City=c.ID) > 1
-
-
-SELECT c.City, co.County, a.FK_City, a.FK_County FROM Address a
-                                                          INNER JOIN City c ON a.FK_City = c.ID
-                                                          INNER JOIN County co ON co.ID = a.FK_County
-WHERE c.ID IN  (SELECT DISTINCT Address.FK_County FROM Address WHERE Address.FK_City=c.ID)
-
--- Hier sieht man beide FK
-SELECT DISTINCT Address.FK_County FROM Address WHERE Address.FK_City=481
-
-SELECT * FROM City WHERE City.City = 'Fiddletown';
-
--- KÖNNTE DIE LÖSUNG SEIN:
-
-SELECT c.City, co.County, a.FK_City, a.FK_County FROM City c
-                                                          INNER JOIN Address a ON a.FK_City = c.ID
-                                                          INNER JOIN County co ON co.ID = a.FK_County
-WHERE  (SELECT COUNT(DISTINCT Address.FK_County) FROM Address WHERE Address.FK_City=c.ID) > 1)
-
-
-
-
-
-SELECT * FROM City;
-
-
-    WHERE a.FK_City IN (SELECT ad.FK_City FROM Address ad WHERE (SELECT COUNT(*) FROM Address WHERE Address.FK_County = ad.FK_County) > 1);
-
-    SELECT a.ID, a.Address, City, State, Zipcode, Longitude, Latitude, County FROM Address  a
-    INNER JOIN City  c ON c.ID  = a.FK_City
-    INNER JOIN State s ON s.ID = a.FK_State
-    INNER JOIN Zipcode z ON a.FK_Zipcode = z.ID
-    INNER JOIN Coordinates co ON a.FK_Coordinates = co.ID
-    INNER JOIN County cou ON a.FK_County = cou.ID
-WHERE a.ID = 21750;
-
-
-SELECT City FROM City WHERE FK
+-- Aufgabe 6
+SELECT HT.HomeType, P2.Price, h.Description  FROM City c
+    LEFT JOIN Address a ON a.FK_City = c.ID
+    LEFT JOIN House h ON h.FK_Address = a.ID
+    LEFT JOIN Properties P on h.FK_Properties = P.ID
+    LEFT JOIN HomeType HT on P.FK_HomeType = HT.ID
+    LEFT JOIN Event E on P.FK_Event = E.ID
+    LEFT JOIN Price P2 on h.FK_Price = P2.ID
+WHERE c.City = 'Parlier' AND E.Event = 'Listed for sale';
